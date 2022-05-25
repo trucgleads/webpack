a025e20e58a8db73aab6e568529263c8cd56af2a
trucnguyen0515 <love.your.smile.0515@gmail.com>
2022-05-25 10:35:55 +0700

    u

---
+++ webpack.config.js
@@ -0,0 +1,69 @@
+const path = require('path');
+const webpack = require('webpack');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    // cac file output
+    entry: './index.js',
+    output: {
+        path: path.join(__dirname, '/dist'),
+        filename: '[name].[contenthash].js',
+        publicPath: '/',
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_module/,
+                loader: 'babel-loader',
+            },
+            {
+                test: /\.(sa|sc|c)ss$/,
+                use: [
+                    // {
+                    //     loader: MiniCssExtractPlugin.loader,
+                    // },
+
+                    "css-loader",
+                    "sass-loader"
+                ],
+            }
+        ],
+    },
+    resolve: {
+        extensions: ['*', '.js', '.jsx']
+    },
+
+    optimization: {
+        // Cache vendor
+        // splitChunks: {
+        //     cacheGroups: {
+        //         vendors: {
+        //             chunks: "all",
+        //             test: /[\\/]node_modules[\\/]/,
+        //             name: 'vendor',
+        //             enforce: true
+        //         },
+        //     }
+        // },
+        // runtimeChunk: 'single',
+    },
+
+    plugins: [
+        // new CleanWebpackPlugin(),
+        // parse template duoi khac thanh HTML
+        // new HtmlWebpackPlugin({
+        //     chunks: ['bundle','themeDark'],
+        //     template: './index.html',
+        //     filename: 'index.html'
+        // }),
+        // Extract CSS ra khoi bundle thanh file rieng
+        // new MiniCssExtractPlugin({
+        //     chunks: ['bundle', 'vendors','themeDark','themeLight'],
+        //     filename: '/[name].css',
+        //     chunkFilename: "[name].css",
+        // }),
+    ],
+}
